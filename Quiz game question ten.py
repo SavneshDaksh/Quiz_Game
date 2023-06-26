3e5b20795afaf2701b4db3c0853f4a96ea4cf4a5
Savnesh Daksh <132660989+SavneshDaksh@users.noreply.github.com>
2023-06-26 17:44:33 +0530

    Add files via upload

---
+++ Quiz game question ten.py
@@ -0,0 +1,378 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# Welcome to the quize game
+
+print("What is your name ?")
+print("A - Acdesh")
+print("B - Savnesh Daksh")
+print("C - Ram")
+print("D - jogesh ")
+
+
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+
+q = input("please choose your answer")
+if q == str(option1):
+    print("you are wrong")
+elif q == str(option2):
+    print("you are right")
+elif q == str(option3):
+    print("you are wrong")
+elif q == str(option4):
+    print("you are wrong")
+else:
+    print("please choose a valid option")
+    
+
+
+# # Question 2
+
+# In[2]:
+
+
+print("fruit name ?")
+print("A - peacock")
+print("B - onion")
+print("C - mango")
+print("D - kite")
+
+
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+
+q = input("please choose your answer")
+if q == str(option1):
+    print("you are wrong")
+elif q == str(option2):
+    print("you are wrong")
+elif q == str(option3):
+    print("you are right")
+elif q == str(option4):
+    print("you are wrong")
+else:
+    print("please choose a valid option")
+    
+
+
+# # Question 3
+
+# In[3]:
+
+
+print("What is the capital of india ?")
+print("A - mumbai")
+print("B - haydrabad")
+print("C - new delhi")
+print("D - chandgarh")
+
+
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+
+q = input("please choose your answer")
+if q == str(option1):
+    print("you are wrong")
+elif q == str(option2):
+    print("you are wrong")
+elif q == str(option3):
+    print("you are right")
+elif q == str(option4):
+    print("you are wrong")
+else:
+    print("please choose a valid option")
+    
+
+
+# # Question 4
+
+# In[4]:
+
+
+print("who is the ceo of google ?")
+print("A - sunder pichai")
+print("B - elon musk")
+print("C - ratan tata")
+print("D - mark zuckerberg")
+
+
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+
+q = input("please choose your answer")
+if q == str(option1):
+    print("you are right")
+elif q == str(option2):
+    print("you are wrong")
+elif q == str(option3):
+    print("you are wrong")
+elif q == str(option4):
+    print("you are wrong")
+else:
+    print("please choose a valid option")
+    
+
+
+# # Question 5
+
+# In[5]:
+
+
+print("who is the ceo of twitter ?")
+print("A - sunder pichai")
+print("B - elon musk")
+print("C - ratan tata")
+print("D - mark zuckerberg")
+
+
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+
+q = input("please choose your answer")
+if q == str(option1):
+    print("you are wrong")
+elif q == str(option2):
+    print("you are right")
+elif q == str(option3):
+    print("you are wrong")
+elif q == str(option4):
+    print("you are wrong")
+else:
+    print("please choose a valid option")
+    
+
+
+# # Question 6
+
+# In[6]:
+
+
+print("who is the founder of amazon ?")
+print("A - sunder pichai")
+print("B - mukesh ambani")
+print("C - ratan tata")
+print("D - jeff bezos")
+
+
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+
+q = input("please choose your answer")
+if q == str(option1):
+    print("you are wrong")
+elif q == str(option2):
+    print("you are wrong")
+elif q == str(option3):
+    print("you are wrong")
+elif q == str(option4):
+    print("you are right")
+else:
+    print("please choose a valid option")
+    
+
+
+# # Question 7
+
+# In[7]:
+
+
+print("Where is the office of skill sequel ?")
+print("A - karnatka ")
+print("B - bihar")
+print("C - uttarpradesh")
+print("D - punjab")
+
+
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+
+q = input("please choose your answer")
+if   q == str(option1):
+    print("you are right")
+elif q == str(option2):
+    print("you are wrong")
+elif q == str(option3):
+    print("you are wrong")
+elif q == str(option4):
+    print("you are wrong")
+else:
+    print("please choose a valid option")
+    
+
+
+# # Question 8
+
+# In[8]:
+
+
+print("who is the chairman of Infosys? ?")
+print("A - ratan tata ")
+print("B - ronaldo")
+print("C - Nandan Nilekani")
+print("D - sunder pichai")
+
+
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+
+q = input("please choose your answer")
+if   q == str(option1):
+    print("you are wrong")
+elif q == str(option2):
+    print("you are wrong")
+elif q == str(option3):
+    print("you are right")
+elif q == str(option4):
+    print("you are wrong")
+else:
+    print("please choose a valid option")
+    
+
+
+# # Question 9
+
+# In[9]:
+
+
+print("http stands for ?")
+print("A - hipertexttranper paper ")
+print("B - HyperText Transfer Protocol")
+print("C - haipertext transper protocol")
+print("D - haypertext transper protocol")
+
+
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+
+q = input("please choose your answer")
+if   q == str(option1):
+    print("you are wrong")
+elif q == str(option2):
+    print("you are right")
+elif q == str(option3):
+    print("you are wrong")
+elif q == str(option4):
+    print("you are wrong")
+else:
+    print("please choose a valid option")
+    
+
+
+# # Question 10
+
+# In[2]:
+
+
+print("ml stands for ?")
+print("A - markup language ")
+print("B - making large")
+print("C - machine lenght")
+print("D - Machine learning")
+
+
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+
+q = input("please choose your answer")
+if   q == str(option1):
+    print("you are wrong")
+elif q == str(option2):
+    print("you are wrong")
+elif q == str(option3):
+    print("you are wrong")
+elif q == str(option4):
+    print("you are right")
+else:
+    print("please choose a valid option")
+    
+
+
+# # Question 11
+
+# In[5]:
+
+
+print("nlp stands for ?")
+print("A - natural large process")
+print("B - natural language processing")
+print("C - natural language processor")
+print("D - neutral language processing")
+
+
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+
+q = input("please choose your answer")
+if   q == str(option1):
+    print("you are wrong")
+elif q == str(option2):
+    print("you are right")
+elif q == str(option3):
+    print("you are wrong")
+elif q == str(option4):
+    print("you are wrong")
+else:
+    print("please choose a valid option")
+
+
+# # Question 12
+
+# In[6]:
+
+
+print("pci stands for ?")
+print("A - natural large process")
+print("B - natural language processing")
+print("C - natural language processor")
+print("D - Peripheral Component Interconnect")
+
+
+option1 = "A"
+option2 = "B"
+option3 = "C"
+option4 = "D"
+
+q = input("please choose your answer")
+if   q == str(option1):
+    print("you are wrong")
+elif q == str(option2):
+    print("you are wrong")
+elif q == str(option3):
+    print("you are wrong")
+elif q == str(option4):
+    print("you are right")
+else:
+    print("please choose a valid option")
+
+
+# In[ ]:
+
+
+
+
